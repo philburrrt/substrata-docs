855b254b0820fa5b49d47a5e9e7d8664ceb9d3a2
philburrrt <75766943+philburrrt@users.noreply.github.com>
2022-03-11 21:06:34 -0600

    redirect landing page to intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-import SearchBar from "@theme-original/SearchBar"
-
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Substrata</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Substrata Docs`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-    </Layout>
-  );
+  return <Redirect to='/docs/user/intro' />;
 }
